14e7b8c90c84293cb919b0f57336ed2534e6d548
Leonardo Coronel <leomcoronel99@gmail.com>
2020-06-05 23:34:23 -0300

    Fixeo numero random
    Al utilizar la función random me excedi por una unidad los valores que puede tomar la variable, por lo que al realizar // result += characters[randInt];  // si el randInt era mayor que a las posiciones que tiene characters, daba undefined.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('#btnChange');
 const body = document.body;
+const hex = document.querySelector('#hex')
 // Event Listeners 
 
 btn.addEventListener('click', cambiarColor);
@@ -12,15 +13,16 @@ function cambiarColor(e) {
 
     for (var i = 0; i < 6; i++) {
         randInt = enteroRandom();
-        result += characters[randInt];
+        result += characters[randInt];   
     }
 
     body.style.backgroundColor = "#" + result;
+    hex.innerHTML = `HEX: ${result}`;
 }
 
 function enteroRandom() {
     let numero;
-    numero = Math.floor(Math.random() * (17 - 0)) + 0;
+    numero = Math.floor(Math.random() * 16);
 
     return numero;
 }
